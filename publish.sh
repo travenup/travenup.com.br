afb1a0103104a62110219394a5e0a7fc82b40f7e
Thiago Hirata <hirata@devcase.com.br>
2015-08-19 17:54:10 -0300

    Atualização do site

--- publish.sh
+++ publish.sh
@@ -3,9 +3,11 @@
 git checkout master
 git fetch
 git merge origin/atualizacaoconteudo
-hugo
 cd public
 git checkout master
+cd ..
+hugo
+cd public
 git add .
 git commit --message "Atualização do site"
 git push
@@ -13,3 +15,20 @@ cd ..
 git add .
 git commit --message "Atualização do site"
 git push
+
+git checkout travenup.com.br
+git merge master
+cd public
+git checkout gh-pages
+cd ..
+hugo
+cd public
+git add .
+git commit --message "Atualização do site"
+git push
+cd ..
+git add .
+git commit --message "Atualização do site"
+git push
+
+
